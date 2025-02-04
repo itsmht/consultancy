142320acc4c7c35e202e6a39bf3fb8ae70985953
itsmht <iamtalha.mht@gmail.com>
2025-02-04 15:17:47 +0600

    bug fix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Middleware\AuthHeader;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
