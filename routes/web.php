5b5b3e2bc5969045ad1e1ba9d0d7969741065142
itsmht <iamtalha.mht@gmail.com>
2025-02-12 22:20:28 +0600

    routes and middleware added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthGuest;
+use App\Http\Middleware\AuthUser;
+use App\Http\Middleware\PreventBack;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+//Auth Routes
+Route::get('/', [AdminController::class, 'login'])->name('login');
+Route::post('/login.submit', [AdminController::class, 'loginSubmit'])->name('login.submit');
+Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
+
+//Admin Routes
+Route::group(['middleware' => [AuthGuest::class, AuthUser::class, PreventBack::class]], function () {
+    Route::get('/adminDashboard', [AdminController::class, 'dashboard'])->name('adminDashboard');
+    Route::get('/categories', [AdminController::class, 'categories'])->name('categories');
+    Route::post('/addCategory', [AdminController::class, 'addCategory'])->name('addCategory');
+    Route::get('/products', [AdminController::class, 'products'])->name('products');
+    Route::get('/addProduct', [AdminController::class, 'addProduct'])->name('addProduct');
 });
