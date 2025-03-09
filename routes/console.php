6a2cd1d7eec224e12784b94f24ca4231c17e684b
itsmht <iamtalha.mht@gmail.com>
2025-03-09 14:12:55 +0600

    added scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\SendPendingMails;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+//Artisan::command('inspire', function () {
+    //$this->comment(Inspiring::quote());
+//})->purpose('Display an inspiring quote');
+
+Schedule::command('send:pending-mails')->everyTwoMinutes();
